93b66fd8473abe3d6956394114a4ecd1cedb6d7a
José A. Rodríguez <joseanrona@gmail.com>
2021-02-17 18:45:34 +0100

    ejercicio 4 terminado

---
+++ e4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ $# -ne 2 ];then
+    echo "Dame solo dos parámetros"
+    exit 1
+else
+    if [ $1 -gt $2 ];then
+        echo $1 es mayor que $2
+    elif [ $2 -gt $1 ];then
+        echo $2 es mayor que $1
+    else
+        echo $1 y $2 son iguales
+    fi
+fi                    
