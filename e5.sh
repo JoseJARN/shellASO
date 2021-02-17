2683513cc849e4c7ba3e44b7830e1c2fe871d399
José A. Rodríguez <joseanrona@gmail.com>
2021-02-17 18:50:16 +0100

    cambios en el 4 y el 5 terminado

--- e5.sh
+++ e5.sh
@@ -5,7 +5,9 @@ if [ $# -ne 2 ];then
 else
     if [ $1 -gt $2 ];then
         echo $1 es mayor que $2
+    elif [ $2 -gt $1 ];then
+        echo $1 es menor que $2
     else
-        echo $2 es mayor que $1
+        echo $1 y $2 son iguales
     fi
-fi                    
+fi 
