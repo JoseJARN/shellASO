12552e07425d38880282cd2e1c8adf991057628d
José A. Rodríguez <joseanrona@gmail.com>
2021-02-18 11:04:37 +0100

    mejora en el ejericio 15

--- e15.sh
+++ e15.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 ls -la
-echo "La fecha de hoy es " 
-date
-echo "El usuario actual es " 
-whoami 
-echo "y mi nombre es Jose"
+echo "La fecha de hoy es " `date`
+echo "El usuario actual es" `whoami` "y mi nombre es Jose"
+
