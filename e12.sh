cbba7c5407e623b31903c9153778330916735b4c
José A. Rodríguez <joseanrona@gmail.com>
2021-02-18 10:38:52 +0100

    arreglamos el ejercicio 12

--- e12.sh
+++ e12.sh
@@ -2,18 +2,16 @@
 if [ $# -eq 0 ];then
     echo "No has pasado ningún parámetro, trata de poner al menos uno"
     exit 1
-fi    
-if [ ! -e $* ];then
-    echo "Los archivos que has enviado no existen"
-    exit 2
-else
-    if [ -f $* ];then
-        echo "Los parámetros que has pasado son ficheros"
-    elif [ -d $* ];then
-        echo "Los parámetros que has pasado son directorios"
-    else
-        echo "Los parámetros que has pasado ni son ficheros ni son directorios"
-    fi
 fi
 
-#Funciona solo cuando paso un parámetro, si paso más de uno me da error.
+for cosa in $*;do
+    if [ ! -e $cosa ];then
+        echo "$cosa no existe"
+    elif [ -d $cosa ];then
+        echo "$cosa es un directorio"
+    elif [ -f $cosa ];then
+        echo "$cosa es un fichero"
+    else
+        echo "Existe pero no es ni un fichero ni un directorio"
+    fi
+done
